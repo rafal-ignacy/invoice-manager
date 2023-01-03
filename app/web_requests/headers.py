48ec0da97ed369eb6342b7345aa73d4c4c89dae1
Rafal Ignacy <rafal.ignacy.99@gmail.com>
2023-01-03 17:59:40 +0100

    Updated access to class variables in headers.py

--- app/web_requests/headers.py
+++ app/web_requests/headers.py
@@ -1,38 +1,33 @@
 from dataclasses import dataclass
 from typing import Dict
-import json
 
 
 from app.settings import ROOT_DIR
+from app.web_requests.request_component import RequestComponent
 
 
 @dataclass
-class Headers:
+class Headers(RequestComponent):
     def __init__(self) -> None:
-        self.headers = self.__get_file_data_dict(ROOT_DIR + r"\app\data\headers.json")
-        self.credentials: Dict = self.__get_file_data_dict(ROOT_DIR + r"\app\data\credentials.json")
+        self.__headers = self.get_file_data_dict(ROOT_DIR + r"\app\data\headers.json")
+        self.__credentials: Dict = self.get_file_data_dict(ROOT_DIR + r"\app\data\credentials.json")
 
     def get_ebay_access_token(self) -> Dict:
-        headers: Dict = self.headers["get_ebay_access_token"]
-        headers["Authorization"] = headers["Authorization"].replace("{base64_encoded_credentials}", self.credentials["ebay_base64_encoded_credentials"])
+        headers: Dict = self.__headers["get_ebay_access_token"]
+        headers["Authorization"] = headers["Authorization"].replace("{base64_encoded_credentials}", self.__credentials["ebay_base64_encoded_credentials"])
         return headers
 
     def get_orders(self, access_token: str) -> Dict:
-        headers: Dict = self.headers["get_orders"]
+        headers: Dict = self.__headers["get_orders"]
         headers["Authorization"] = headers["Authorization"].replace("{ebay_access_token}", access_token)
         return headers
 
     def get_order(self, access_token: str) -> Dict:
-        headers: Dict = self.headers["get_order"]
+        headers: Dict = self.__headers["get_order"]
         headers["Authorization"] = headers["Authorization"].replace("{ebay_access_token}", access_token)
         return headers
 
     def create_invoice(self) -> Dict:
-        headers: Dict = self.headers["create_invoice"]
-        headers["ApiUserCompanyRoleKey"] = headers["ApiUserCompanyRoleKey"].replace("{ing_api_key}", self.credentials["ing_api_key"])
+        headers: Dict = self.__headers["create_invoice"]
+        headers["ApiUserCompanyRoleKey"] = headers["ApiUserCompanyRoleKey"].replace("{ing_api_key}", self.__credentials["ing_api_key"])
         return headers
-
-    def __get_file_data_dict(self, path: str) -> Dict:
-        with open(path, "r") as file:
-            file_data_dict = json.loads(file.read())
-        return file_data_dict
