f6a2f1f0dbe82ae2ff37364250f283cd7f83f1ec
unknown <rafal.ignacy.99@gmail.com>
2023-01-06 23:48:07 +0100

    Updated urls.py file

--- app/web_requests/urls.py
+++ app/web_requests/urls.py
@@ -8,20 +8,20 @@ from app.web_requests.request_component import RequestComponent
 @dataclass
 class Urls(RequestComponent):
     def __init__(self) -> None:
-        self.urls = self.get_file_data_dict(ROOT_DIR + r"\app\data\urls.json")
+        self.__urls = self.get_file_data_dict(ROOT_DIR + r"\app\data\urls.json")
 
-    def get_access_token(self) -> str:
-        url: str = self.urls["get_access_token"]
+    def get_ebay_access_token(self) -> str:
+        url: str = self.__urls["get_ebay_access_token"]
         return url
 
     def get_order(self, order_id: str) -> str:
-        url: str = self.urls["get_order"].replace("{order_id}", order_id)
+        url: str = self.__urls["get_order"].replace("{order_id}", order_id)
         return url
 
     def get_orders(self, date: str) -> str:
-        url: str = self.urls["get_orders"].replace("{date}", date)
+        url: str = self.__urls["get_orders"].replace("{date}", date)
         return url
 
     def create_invoice(self) -> str:
-        url: str = self.urls["create_invoice"]
+        url: str = self.__urls["create_invoice"]
         return url
