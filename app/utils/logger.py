da3463fc7af21561cc9ac66f01712c4b8fb6a484
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-01-10 20:56:30 +0100

    Updated logger.py file

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -3,7 +3,7 @@ import logging
 
 class Logger:
     def __init__(self):
-        logging.basicConfig(filename="log", level=logging.INFO, format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+        logging.basicConfig(filename="logs/log", level=logging.INFO, format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
     def info(self, message):
         logging.info(message)
