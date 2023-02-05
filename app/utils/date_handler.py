96b843ac4e1c07b4437c84a391cd6fbaae70bb83
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-02-05 13:45:40 +0100

    Modified date_handler.py file

--- app/utils/date_handler.py
+++ app/utils/date_handler.py
@@ -2,7 +2,12 @@ from datetime import datetime, timedelta
 
 
 class DateHandler:
-    def get_orders_utc_delta(self, hour_difference) -> str:
-        utc_now = datetime.utcnow()
-        utc_subtracted = utc_now - timedelta(hours=hour_difference)
+    def get_orders_utc_delta(self, hour_difference: int) -> str:
+        utc_now: datetime = datetime.utcnow()
+        utc_subtracted: datetime = utc_now - timedelta(hours=hour_difference)
         return utc_subtracted.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+    def convert_date(self, date: str) -> str:
+        datetime_object: datetime = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+        datetime_converted: str = datetime.strftime(datetime_object, "%d/%m/%Y %H:%M:%S")
+        return datetime_converted
