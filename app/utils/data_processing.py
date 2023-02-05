d868914e7c35aba83a0d9d2386bf015068326fc5
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-02-05 13:44:26 +0100

    Added data_processing.py file

---
+++ app/utils/data_processing.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import List, Tuple
+
+from app.data_models.item_details import ItemDetails
+from app.utils.date_handler import DateHandler
+
+
+@dataclass
+class DataProcessing:
+    def convert_dates(self, order_date: str, payment_date: str | None):
+        date_handler: DateHandler = DateHandler()
+        order_date = date_handler.convert_date(order_date[:-5])
+        if payment_date is not None:
+            payment_date = date_handler.convert_date(payment_date[:-5])
+        return order_date, payment_date
+
+    def convert_types(self, total: str | float, delivery_total: str | float, items_list: List[ItemDetails]) -> Tuple[float, float]:
+        total, delivery_total = float(total), float(delivery_total)
+        for item in items_list:
+            item.price = float(item.price)
+        return total, delivery_total
+
+    def convert_sku(self, items_list: List[ItemDetails]):
+        for item in items_list:
+            if item.sku is not None:
+                item.sku = item.sku[:-3]
