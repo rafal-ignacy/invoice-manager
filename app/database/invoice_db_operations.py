ddf680a4d6b35cdb22dc2eb515fab342c8a0233a
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-02-05 13:42:13 +0100

    Added database files

---
+++ app/database/invoice_db_operations.py
@@ -0,0 +1,38 @@
+from typing import List
+from dataclasses import dataclass
+
+from app.database.db import Database
+from app.database.db_operations import DatabaseOperations
+
+
+@dataclass
+class InvoiceDatabaseOperations(DatabaseOperations):
+    def __init__(self) -> None:
+        self.queries_data = self.get_queries_data()
+
+    def get_details_of_paid_orders_without_invoice(self) -> List:
+        SQL_query: str = self.queries_data["get_details_of_paid_orders_without_invoice"]
+        with Database(function_name="Get details of paid orders without invoice") as database:
+            database.cursor.execute(SQL_query)
+            database_result: List = database.cursor.fetchall()
+
+        orders_list = []
+        for database_order_data in database_result:
+            order_details = []
+            order_details.append(database_order_data)
+            orders_list.append(order_details)
+        return orders_list
+
+    def get_customer_details(self, customer_id: int):
+        SQL_query: str = self.queries_data["get_customer_details"].replace("{customer_id}", str(customer_id))
+        with Database(function_name="Get customer details") as database:
+            database.cursor.execute(SQL_query)
+            result: List = database.cursor.fetchall()[0]
+        return result
+
+    def get_items_details(self, order_id: int):
+        SQL_query: str = self.queries_data["get_items_details"].replace("{order_id}", str(order_id))
+        with Database(function_name="Get items details") as database:
+            database.cursor.execute(SQL_query)
+            result: List = database.cursor.fetchall()
+        return result
