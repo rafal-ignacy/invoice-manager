49fae89d81340ed7d9d5ff30090311cb51196c6a
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-02-05 19:52:13 +0100

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from app.utils.date_handler import DateHandler
 from app.web_requests.web_requests import WebRequests
-from app.utils.order_data_handler import OrderDataHandler
+from app.utils.response_handler import ResponseHandler
+from app.orders_processing.orders_processing import OrdersProcessing
+from app.invoice_processing.invoice_processing import InvoiceProcessing
 
 if __name__ == "__main__":
     date_handler = DateHandler()
-    utc_time = date_handler.get_orders_utc_delta(12)
+    utc_time = date_handler.get_orders_utc_delta(480)
+
     web_requests = WebRequests()
+    request_handler = ResponseHandler()
     access_token_response = web_requests.get_ebay_access_token()
-    access_token = access_token_response["access_token"]
-    get_order_response = web_requests.get_order("08-09558-08033", access_token)
-    order_data_handler = OrderDataHandler()
-    response = order_data_handler.get_details(get_order_response)
+    access_token = request_handler.get_ebay_access_token(access_token_response)
+    request_response = web_requests.get_orders(utc_time, access_token)
+    ebay_orders_list = request_handler.get_orders(request_response)
+
+    orders_processing = OrdersProcessing()
+    orders_processing.get_orders_data(ebay_orders_list)
+    orders_processing.check_orders_existence_in_database()
+    orders_processing.prepare_orders_data_to_database()
+    orders_processing.add_orders_to_database()
+
+    invoice_processing = InvoiceProcessing()
+    invoice_processing.get_orders_data_for_creating_invoices()
+    invoice_processing.check_sku_existence_in_orders_items()
