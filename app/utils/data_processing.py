9df10431c32fe0eba38515afa546dc17bd508fb2
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-02-16 20:39:21 +0100

    Modified data_processing.py

--- app/utils/data_processing.py
+++ app/utils/data_processing.py
@@ -9,9 +9,9 @@ from app.utils.date_handler import DateHandler
 class DataProcessing:
     def convert_dates(self, order_date: str, payment_date: str | None):
         date_handler: DateHandler = DateHandler()
-        order_date = date_handler.convert_date(order_date[:-5])
+        order_date = date_handler.convert_date_from_ebay_format(order_date[:-5])
         if payment_date is not None:
-            payment_date = date_handler.convert_date(payment_date[:-5])
+            payment_date = date_handler.convert_date_from_ebay_format(payment_date[:-5])
         return order_date, payment_date
 
     def convert_types(self, total: str | float, delivery_total: str | float, items_list: List[ItemDetails]) -> Tuple[float, float]:
